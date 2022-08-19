55f359a8ed31a7c975082c29ddc395023adddfd5
Matt Smith <matt.smith3108@gmail.com>
2022-08-19 10:58:44 +0100

    Apollo client setup

---
+++ apollo/client.js
@@ -0,0 +1,33 @@
+import {
+  ApolloClient,
+  InMemoryCache,
+  gql,
+  createHttpLink,
+} from "@apollo/client";
+
+const defaultOptions = {
+  watchQuery: {
+    fetchPolicy: "no-cache",
+    errorPolicy: "ignore",
+  },
+  query: {
+    fetchPolicy: "no-cache",
+    errorPolicy: "all",
+  },
+};
+
+const cache = new InMemoryCache({
+  resultCaching: false,
+});
+
+const link = createHttpLink({
+  uri: `${process.env.NEXT_PUBLIC_WORDPRESS_SITE_URL}/graphql`,
+});
+
+const client = new ApolloClient({
+  link,
+  cache,
+  defaultOptions,
+});
+
+export default client;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import Link from 'next/link'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
 
 const Home: NextPage = () => {
   return (
@@ -12,14 +12,14 @@ const Home: NextPage = () => {
       </Head>
 
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-       <h1 className='text-3xl pb-10'>Welcome to our demo blog!</h1>
+        <h1 className="text-3xl pb-10">Welcome to our demo blog!</h1>
 
-       <p>
-        You can find more articles on the {' '}
-        <Link href='/blog'>
-          <a>blog articles page</a>
-        </Link>
-       </p>
+        <p>
+          You can find more articles on the{" "}
+          <Link href="/blog">
+            <a>blog articles page</a>
+          </Link>
+        </p>
       </main>
 
       <footer className="flex h-24 w-full items-center justify-center border-t">
@@ -29,18 +29,12 @@ const Home: NextPage = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
         </a>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
-
-export async function getStaticProps() {
-
-  
-
-}
+export default Home;
